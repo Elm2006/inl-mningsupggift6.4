2b8fe8a44a8ea929a0e1e65a163f75da8bac7875
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2024-02-14 10:59:19 +0100

    Update Program.cs

--- inlämningsupggift6.4/Program.cs
+++ inlämningsupggift6.4/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Skriv in ett tal");
-        int a = int.Parse(Console.ReadLine());
+        double a = double.Parse(Console.ReadLine());
         Console.WriteLine("Skriv in ett tal");
-        int b = int.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
         Console.WriteLine(" talet "+a+" upphöjt med "+b+" = "+ potens(a,b));
     }
-    static int potens(int a, int b)
+    static double potens(double a, double b)
     {
-        int p = a;
-        for (int i = 1; i <b;i++)
+        double p = a;
+        for (double i = 1; i <b;i++)
         {
             p *= a;
         }
