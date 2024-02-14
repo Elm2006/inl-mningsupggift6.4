c62059c22ab889a1ed440e87acdc8186cd2991cb
Elm2006 <isel06002@utb.vaxjo.se>
2024-02-14 10:56:22 +0100

    Add project files.

---
+++ inlämningsupggift6.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett tal");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Skriv in ett tal");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(" talet "+a+" upphöjt med "+b+" = "+ potens(a,b));
+    }
+    static int potens(int a, int b)
+    {
+        int p = a;
+        for (int i = 1; i <b;i++)
+        {
+            p *= a;
+        }
+        return p;
+    }
+}
